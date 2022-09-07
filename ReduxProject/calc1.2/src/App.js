5789158ae2d4997f5a468b2a83066e1dd33e3ba9
DavidSheinkman <davidsheinkman7@gmail.com>
2022-09-07 06:00:00 +0300

    projects

--- ReduxProject/calc1.2/src/App.js
+++ ReduxProject/calc1.2/src/App.js
@@ -1,6 +1,7 @@
 import { Fragment, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
