a1a3ea683585419dc1678f13237c02d7d271ccf1
DavidSheinkman <davidsheinkman7@gmail.com>
2023-05-25 02:38:55 +0300

    new

--- betterWeb3mSol/src/App.js
+++
@@ -1,7 +0,0 @@
-import Food from "./pages/Food";
-
-function App() {
-  return <Food />;
-}
-
-export default App;
--- web3mtask/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import btncssSlice from './btncss-slice';
-
-const store = configureStore({
-  reducer: {  btncss: btncssSlice.reducer },
-});
-
-export default store;
